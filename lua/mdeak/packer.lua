ef239f17d5cd827ecebeb2ba3c0eccd7411f59a1
Matthew Deakos <mwoolnderdeakos@gmail.com>
2023-03-20 11:21:06 -0300

    use make for fzf

--- lua/mdeak/packer.lua
+++ lua/mdeak/packer.lua
@@ -62,6 +62,5 @@ return require('packer').startup(function(use)
     use 'jose-elias-alvarez/null-ls.nvim'
     use 'github/copilot.vim'
 
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run =
-    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 end)
