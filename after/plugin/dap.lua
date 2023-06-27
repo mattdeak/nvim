50dbdbfa0bfd9b50edcf66838328eabdea8a4857
Matthew Deakos <mwoolnerdeakos@gmail.com>
2023-06-27 15:57:11 -0300

    some dap stuff

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -6,6 +6,11 @@ local dap, dapui = require("dap"), require("dapui")
 dap_python.test_runner = "pytest"
 dap_python.setup("~/.virtualenvs/debugpy/bin/python")
 
+-- If we are ina python file, set the debugpy adapter keymaps
+vim.keymap.set("n", "<leader>dt", "<cmd>lua require'dap-python'.test_method()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dc", "<cmd>lua require'dap-python'.test_class()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>df", "<cmd>lua require'dap-python'.test_file()<CR>", { noremap = true, silent = true })
+
 -- UI
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
@@ -25,7 +30,7 @@ dapui.setup()
 -- Remaps
 vim.keymap.set("n", "<F5>", "<cmd>lua require'dap'.continue()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<F10>", "<cmd>lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<F11>", "<cmd>lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<F2>", "<cmd>lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<F12>", "<cmd>lua require'dap'.step_out()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>b", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>B", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
--- lua/mdeak/init.lua
+++ lua/mdeak/init.lua
@@ -1,3 +1,11 @@
 require("mdeak.remap")
 require("mdeak.set")
 
+local vim = vim
+local opt = vim.opt
+
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+opt.foldnestmax = 10
+opt.foldminlines = 1
